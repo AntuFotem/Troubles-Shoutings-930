96bb5bddb5a94bfdcf7ea0f7c11c1bdf5b0b9c7e
Hernán González <131327497+AntuFotem@users.noreply.github.com>
2024-05-10 11:31:48 -0400

    cambio de contraseña a una mas segura

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ formulario.addEventListener('submit', (event) => {
   const contrasena = document.getElementById('contrasena').value;
 
   // Validación básica (reemplazar)
-  if (usuario === "admin" && contrasena === "1234") {
+  if (usuario === "admin" && contrasena === "6AkIBtoZvJ") {
     // Obtener datos de usuario (ejemplo ficticio)
     const nombreUsuario = "Juan Pérez";
     const datosUsuario = JSON.stringify({ // Serializar datos en formato JSON
